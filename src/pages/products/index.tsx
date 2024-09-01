a2aa2f19556c2df323d19f00176e0752d16c791d
Ding Ruoqian <bubuding0809@gmail.com>
2024-09-01 23:32:07 +0800

    Shortened sheet description phrasing

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -685,8 +685,7 @@ const ProductCardCustomer = ({
                         <SheetTitle>Customer Product Pricing</SheetTitle>
                         <SheetDescription>
                           Set a default price for this product that will
-                          automatically apply to orders from this specific
-                          customer
+                          automatically apply to this customer’s orders
                         </SheetDescription>
                       </SheetHeader>
                       {/* Product preview */}
